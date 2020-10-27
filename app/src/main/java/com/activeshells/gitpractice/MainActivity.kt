2fdc30cba7a695bac39cdff108d1391e9ea67e2e
Prashanth K S <prashanth.siteurl@gmail.com>
2020-10-27 16:59:44 +0530

    Revert "Fifth Commit"
    This reverts commit 64ae522d

--- app/src/main/java/com/activeshells/gitpractice/MainActivity.kt
+++ app/src/main/java/com/activeshells/gitpractice/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.activeshells.gitpractice
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("Second Commit")
-        println("Third Commit")
     }
 }
