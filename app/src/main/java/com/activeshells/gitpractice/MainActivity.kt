7fb7194a9db20673f066c221c3327c0eaaed5bc4
Prashanth K S <prashanth.siteurl@gmail.com>
2020-10-27 17:49:32 +0530

    Test Commit

--- app/src/main/java/com/activeshells/gitpractice/MainActivity.kt
+++ app/src/main/java/com/activeshells/gitpractice/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("Second Commit")
         print("Experiment")
+        print("Test Commit")
     }
 }
